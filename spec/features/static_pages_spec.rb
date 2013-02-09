6b9bc22167324a61e9ada4ceb3c6093450e67b2b
Kori Roys <kori@koriroys.com>
2013-02-08 18:49:42 -0600

    chapter three contact page challenge complete

--- spec/features/static_pages_spec.rb
+++ spec/features/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
+
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
@@ -12,7 +14,7 @@ describe "Static pages" do
     it "should have the title 'Home'" do
       visit static_pages_home_path
       expect(page).to have_selector 'title',
-        text: 'Ruby on Rails Tutorial Sample App | Home'
+        text: "#{base_title} | Home"
     end
   end
 
@@ -26,7 +28,7 @@ describe "Static pages" do
     it "should have the title 'Help'" do
       visit static_pages_help_path
       expect(page).to have_selector 'title',
-        text: 'Ruby on Rails Tutorial Sample App | Help'
+        text: "#{base_title} | Help"
     end
   end
 
@@ -39,7 +41,20 @@ describe "Static pages" do
     it "should have the title 'About Us'" do
       visit static_pages_about_path
       expect(page).to have_selector 'title',
-        text: 'Ruby on Rails Tutorial Sample App | About Us'
+        text: "#{base_title} | About Us"
+    end
+  end
+
+  describe 'Contact page' do
+    it "should have the content 'Contact'" do
+      visit static_pages_contact_path
+      expect(page).to have_selector 'h1', text: 'Contact'
+    end
+
+    it "should have the title 'Contact'" do
+      visit static_pages_contact_path
+      expect(page).to have_selector 'title',
+        text: "#{base_title} | Contact"
     end
   end
 end
